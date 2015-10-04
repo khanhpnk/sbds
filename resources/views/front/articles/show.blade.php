ffe1654ea74fb05d05d1a384cc15b600685da3e2
Rukan <phamngockhanh262@gmail.com>
2015-10-05 03:42:48 +0900

    aaa

--- resources/views/front/articles/show.blade.php
+++ resources/views/front/articles/show.blade.php
@@ -8,14 +8,17 @@
 @stop
 
 @section('content')
-  <article>
-    <h1>{{ $article->title }}</h1>
+  <article class="article">
+    <header><h1 class="article-title">{{ $article->title }}</h1></header>
     {!! $article->description !!}
   </article>
 
   <hr>
+  <div class="fb-comments" data-href="{{ route('front.article.show', ['slug' => $article->slug]) }}" data-width="675" data-numposts="2"></div>
+  <hr>
 
   <section class="relation">
+    <header><h1 class="relation-title">Các tin khác</h1></header>
     <ul>
       @foreach ($relations as $relation)
         <li><a href="{{ route('front.article.show', ['slug' => $relation->slug]) }}"><i class="fa fa-link"></i>{{ $relation->title }}</a></li>
