3c7817995f929028138604d5955908d5d7cc8555
Khanh <phamngockhanh262@gmail.com>
2015-10-08 12:24:48 +0900

    aaaaaaaaaa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Project;
 use App\House;
+use HouseSaleOption;
 
 class HomeController extends Controller
 {
     public function index()
     {
-//        $projects = Project::orderBy('id', 'desc')->expired(false)->simplePaginate(3);
-//        $housesSale = House::orderBy('id', 'desc')->isSale(1)->expired(false)->simplePaginate(3);
-//        $housesRent = House::orderBy('id', 'desc')->isSale(2)->expired(false)->simplePaginate(3);
-        return view('home.index', compact('projects', 'housesSale', 'housesRent'));
+        $projects = Project::orderBy('id', 'desc')->expired(false)->simplePaginate(3);
+        $housesFeatured = House::orderBy('id', 'desc')->expired(false)->simplePaginate(3);
+        $housesSale = House::orderBy('id', 'desc')->isSale(HouseSaleOption::BAN)->expired(false)->simplePaginate(3);
+        $housesRent = House::orderBy('id', 'desc')->isSale(HouseSaleOption::CHO_THUE)->expired(false)->simplePaginate(3);
+        
+        return view('home.index', compact('housesFeatured', 'projects', 'housesSale', 'housesRent'));
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,11 +2,80 @@
 
 @section('content')
 
-	{{--@include('home._house_featured')--}}
-	{{--@include('home._house_section', ['title' => 'Tin mới', 'viewMoreRoute' => ''])--}}
-	{{--@include('home._house_project_featured')--}}
-	{{--@include('home._house_section', ['title' => 'Nhà đất bán', 'viewMoreRoute' => ''])--}}
-	{{--@include('home._house_section', ['title' => 'Nhà đất cho thuê', 'viewMoreRoute' => ''])--}}
-	{{--@include('home._house_section', ['title' => 'Dự án', 'viewMoreRoute' => ''])--}}
+	@if (0 < count($housesFeatured))
+		<section class="list">
+			<header><h1 class="thumb-title">Tin nổi bật</h1></header>
+			<div class="thumb thumb-br-default clearfix">
+				<div class="row">
+					@foreach ($housesFeatured as $house)
+						@include('houses._article', ['model' => $house, 'resource' => ResourceOption::NHA_DAT])
+					@endforeach
+				</div>
+
+				<a class="btn btn-main" href="{{ route("house.index", ['type' => 'ban']) }}" role="button"><i class="fa fa-plus-square-o"></i> Xem thêm</a>
+			</div>
+		</section>
+	@endif
+
+	@if (0 < count($projects))
+		<section class="list">
+			<header><h1 class="thumb-title">Dự án nổi bật</h1></header>
+			<div class="thumb thumb-br-default clearfix">
+				<div class="row">
+					@foreach ($projects as $project)
+						@include('projects._article', ['model' => $project, 'resource' => ResourceOption::DU_AN])
+					@endforeach
+				</div>
+
+				<a class="btn btn-main" href="{{ route("house.index", ['type' => 'cho-thue']) }}" role="button"><i class="fa fa-plus-square-o"></i> Xem thêm</a>
+			</div>
+		</section>
+	@endif
+
+	@if (0 < count($housesSale))
+		<section class="list">
+			<header><h1 class="thumb-title">Nhà đất bán</h1></header>
+			<div class="thumb thumb-br-default clearfix">
+				<div class="row">
+					@foreach ($housesSale as $house)
+						@include('houses._article', ['model' => $house, 'resource' => ResourceOption::NHA_DAT])
+					@endforeach
+				</div>
+
+				<a class="btn btn-main" href="{{ route("house.index", ['type' => 'ban']) }}" role="button"><i class="fa fa-plus-square-o"></i> Xem thêm</a>
+			</div>
+		</section>
+	@endif
+
+	@if (0 < count($housesRent))
+		<section class="list">
+			<header><h1 class="thumb-title">Nhà đất cho thuê</h1></header>
+			<div class="thumb thumb-br-default clearfix">
+				<div class="row">
+					@foreach ($housesRent as $house)
+						@include('houses._article', ['model' => $house, 'resource' => ResourceOption::NHA_DAT])
+					@endforeach
+				</div>
+
+				<a class="btn btn-main" href="{{ route("house.index", ['type' => 'cho-thue']) }}" role="button"><i class="fa fa-plus-square-o"></i> Xem thêm</a>
+			</div>
+		</section>
+	@endif
+
+	@if (0 < count($projects))
+		<section class="list">
+			<header><h1 class="thumb-title">Dự án</h1></header>
+			<div class="thumb thumb-br-default clearfix">
+				<div class="row">
+					@foreach ($projects as $project)
+						@include('projects._article', ['model' => $project, 'resource' => ResourceOption::DU_AN])
+					@endforeach
+				</div>
+
+				<a class="btn btn-main" href="{{ route("house.index", ['type' => 'cho-thue']) }}" role="button"><i class="fa fa-plus-square-o"></i> Xem thêm</a>
+			</div>
+		</section>
+	@endif
+
 	{{--@include('home._construction_design')--}}
 @stop
