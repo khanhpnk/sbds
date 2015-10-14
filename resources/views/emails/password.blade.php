ecc1af05ff8b347469c80fe39da65fd2e806c959
Khanh <phamngockhanh262@gmail.com>
2015-10-14 21:20:16 +0900

    aaaaaa

---
+++ resources/views/emails/password.blade.php
@@ -0,0 +1,5 @@
+Nhấn vào link sau để thiết lập lại mật khẩu: {{ url('password/reset/'.$token) }}
+<br>
+Link chỉ có hiệu lực trong vòng 1h.
+<br>
+Bạn nhận được email này vì bạn đã đăng ký thiết lập lại mật khẩu. Nếu bạn không thực hiện hành động đó, vui lòng bỏ qua email này.
