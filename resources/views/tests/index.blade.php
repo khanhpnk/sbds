bf952e3b53fe5680adb09d25110b13a70c3f1554
Rukan <phamngockhanh262@gmail.com>
2015-10-19 21:38:38 +0900

    a

--- resources/views/tests/index.blade.php
+++ resources/views/tests/index.blade.php
@@ -1,23 +1,72 @@
-@extends('layout')
-
-@section('javascript')
-	@parent
-	<script>
-		$(function() {
-			$('#testBtn').click(function() {
-				$.ajax({
-					url: "http://ec2-52-74-174-89.ap-southeast-1.compute.amazonaws.com/test",
-					type: "POST",
-					dataType: 'json',
-					data: {mydata : locationStorageJSON}
-				}).done(function(data) {
-					console.log(data);
+<!DOCTYPE html>
+	<html>
+	<head>
+		@include('_head')
+		@section('style')
+		@show
+		@section('jshead')
+		@show
+		<script>
+			$(function() {
+				$('#testBtn').click(function() {
+					$.ajax({
+						url: "http://house360.vn/test",
+						type: "POST",
+						dataType: 'json',
+						data: {mydata : locationStorageJSON}
+					}).done(function(data) {
+						console.log(data);
+					});
 				});
 			});
-		});
-	</script>
-@endsection
+		</script>
+	</head>
+	<body>
+	{{--Initialize the JavaScript SDK--}}
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.4&appId=674952055924751";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
 
-@section('content')
-	<button type="button" class="btn btn-primary" id="testBtn">Test</button>
-@stop
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			@include('_nav')
+		</div>
+	</nav>
+
+	<section class="map-container">
+		@include('_map')
+	</section>
+
+	<section class="breadcrumb-container">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="/">Trang chủ</a></li>
+				@yield('breadcrumb')
+			</ol>
+		</div>
+	</section>
+
+	<div class="container">
+		<div class="row">
+			<main class="col-md-9 main">
+				<button type="button" class="btn btn-primary" id="testBtn">Test</button>
+			</main>
+		</div>
+	</div>
+
+	<footer class="footer">
+		<div class="container">
+			@include('_footer')
+		</div>
+	</footer>
+
+	@include('_jsfooter')
+	@section('javascript')
+	@show
+	</body>
+	</html>
