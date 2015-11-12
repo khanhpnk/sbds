e6a594182515d891878c6e76725dc9ccca40a7d8
Rukan <phamngockhanh262@gmail.com>
2015-11-12 21:34:19 +0900

    sdfgh

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -1,60 +1,79 @@
 <ul class="row">
-  <li class="col-md-3">
+  <li class="col-md-15">
     <ul>
-      <li class="footer-header">
-        <a href="/" style="padding: 0">
-          <img width="40" height="40" src="{{ url('images/logo.png') }}" alt="House360.vn">
-        </a>
-      </li>
-      <li><a href="javascript:void(0)"><i class="fa fa-home"></i>Ngõ 105 Láng Hạ, Đống Đa, Hà Nội</a></li>
-      <li><a href="javascript:void(0)"><i class="fa fa-phone-square"></i>04 66 836 456 - 0983 097 911</a></li>
-      <li><a href="javascript:void(0)"><i class="fa fa-envelope"></i>luongduy.hung@hoalanjsc.com</a></li>
-      <li><a href="javascript:void(0)"><i class="fa fa-fax"></i>(305) 555-4447</a></li>
-      <li><a href="javascript:void(0)"><i class="fa fa-facebook-official"></i>viethome360</a></li>
-      <li><a href="javascript:void(0)"><i class="fa fa-skype"></i>luongduyhung</a></li>
-      <li><a href="http://hoalanstudies.edu.vn" target="_blank"><i class="fa fa-globe"></i>http://hoalanstudies.edu.vn</a></li>
+      <li class="footer-header">HOUSE360</li>
+      <li><a href="#"><i class="fa fa-link"></i>Giới thiệu về house360</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Nội quy đăng tin</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Hướng dãn đăng tin</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Báo giá</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Tuyển dụng</a></li>
     </ul>
   </li>
-  <li class="col-md-3">
+  <li class="col-md-15">
     <ul>
-      <li class="footer-header">Tìm kiếm nhanh</li>
-      <li><a href=""><i class="fa fa-link"></i>Giới thiệu về house</a></li>
-      <li><a href="#"><i class="fa fa-link"></i>Danh sách công ty môi giới</a></li>
-      <li><a href="#"><i class="fa fa-link"></i>Tin nổi bật</a></li>
-      <li><a href="#"><i class="fa fa-link"></i>Dự án nổi bật</a></li>
+      <li class="footer-header">Dịch Vụ Thiết Kế</li>
+      <li><a href="#"><i class="fa fa-link"></i>Biệt thự phố</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Biệt thự vườn</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Nhà phố</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Hiện đại</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Cổ điển</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Căn hộ</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Báo giá thiết kế</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Dịch vụ thiết kế nhà</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Dịch vụ thiết kế nội thất</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Dịch vụ xây và sửa nhà</a></li>
+    </ul>
+  </li>
+  <li class="col-md-15">
+    <ul>
+      <li class="footer-header">Nhà Đất Bán</li>
       <li><a href="#"><i class="fa fa-link"></i>Nhà riêng</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Căn hộ</a></li>
       <li><a href="#"><i class="fa fa-link"></i>Nhà biệt thự, liền kề</a></li>
       <li><a href="#"><i class="fa fa-link"></i>Nhà mặt phố</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Đất nền dự án</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Đất</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Kho, nhà xưởng</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Trang trại, khu nghỉ dưỡng</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Thể loại khác</a></li>
     </ul>
   </li>
-  <li class="col-md-3">
+  <li class="col-md-15">
     <ul>
-      <li class="footer-header">Danh sách hữu ích</li>
-      <li><a href="#"><i class="fa fa-link"></i>Nhà đất bán</a></li>
-      <li><a href="#"><i class="fa fa-link"></i>Nhà đất cho thuê</a></li>
-      <li><a href="#"><i class="fa fa-link"></i>Dự án</a></li>
-      <li><a href="#"><i class="fa fa-link"></i>Thiết kế thi công</a></li>
-      <li><a href=""><i class="fa fa-link"></i>Báo giá</a></li>
-      <li><a href=""><i class="fa fa-link"></i>Tuyển dụng</a></li>
-      <li><a href=""><i class="fa fa-link"></i>Nội quy đăng tin</a></li>
-      <li><a href=""><i class="fa fa-link"></i>Hướng dẫn sử dụng</a></li>
-      <li><a href=""><i class="fa fa-link"></i>Hướng dẫn thanh toán</a></li>
+      <li class="footer-header">Nhà Đất Cho Thuê</li>
+      <li><a href="#"><i class="fa fa-link"></i>Nhà riêng</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Căn hộ</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Nhà biệt thự, liền kề</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Nhà mặt phố</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Đất nền dự án</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Đất</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Kho, nhà xưởng</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Nhà trọ</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Văn phòng</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Kiot, cửa hàng</a></li>
     </ul>
   </li>
-  <li class="col-lg-3">
-    {{--<ul>--}}
-      {{--<li class="footer-header">Danh sách hữu ích</li>--}}
-      {{--<li><a href="{{ route('front.article.show', ['slug' => 'noi-quy-dang-tin']) }}"><i class="fa fa-link"></i>Nội quy đăng tin</a></li>--}}
-      {{--<li><a href="{{ route('front.article.show', ['slug' => 'huong-dan-dang-tin']) }}"><i class="fa fa-link"></i>Hướng dẫn đăng tin</a></li>--}}
-      {{--<li><a href="{{ route('front.article.show', ['slug' => 'thiet-ke-thi-cong']) }}"><i class="fa fa-link"></i>Thiết kế thi công</a></li>--}}
-      {{--<li><a href="{{ route('front.article.show', ['slug' => 'bao-gia-thiet-ke-thi-cong']) }}"><i class="fa fa-link"></i>Báo giá thiết kế thi công</a></li>--}}
-      {{--<li><a href="{{ route('front.article.show', ['slug' => 'tuyen-dung']) }}"><i class="fa fa-link"></i>Tuyển dụng</a></li>--}}
-    {{--</ul>--}}
+  <li class="col-md-15">
+    <ul>
+      <li class="footer-header">Dự Án</li>
+      <li><a href="#"><i class="fa fa-link"></i>Thương mại dịch vụ</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Du lịch nghỉ dưỡng</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Căn hộ chung cư</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Văn phòng cao ốc</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Khu công nghiệp</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Khu đô thị mới</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Khu phức hợp</a></li>
+      <li><a href="#"><i class="fa fa-link"></i>Khu dân cư</a></li>
+    </ul>
   </li>
 </ul>
 
 <div class="footer-site-generator">
-  <span>Bản quyền 2015 Newhouse360.com<span>
+  <p class="footer-site-generator-title">CÔNG TY CP XÂY DỰNG VÀ PHÁT TRIỂN THƯƠNG HIỆU HOA LAN</p>
+  <p>Copyright © 2010 - 2015 House360.vn ® Ghi rõ nguồn 'House360.vn' khi phát hành lại thông tin từ website này.<p>
+  <p>Ngõ 105 Láng Hạ, Đống Đa, Hà Nội</p>
+  <p>Hotline: 04 66 836 456 | 0983600123 - Email: hoúe360.vn@gmail.com</p>
+
   <div class="social-container footer-social">
     <a class="social-facebook" target="_blank" href="https://www.facebook.com/House360-402698543267613/timeline"></a>
     <a class="social-tweet" target="_blank" href="https://twitter.com/House360Vn"></a>
