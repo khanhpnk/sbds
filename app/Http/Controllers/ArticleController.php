4c02c106f4ddbab2822af2cb135936d7ebd5bbf1
Rukan <phamngockhanh262@gmail.com>
2016-01-06 22:00:01 +0900

    h

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Lang;
 use App\Article;
 use Illuminate\Support\Facades\Redirect;
+use App\Admin\BaseController;
 
 class ArticleController extends BaseController
 {
